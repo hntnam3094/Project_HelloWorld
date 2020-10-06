c599680a33b231b2d99d3ed4edcab4b9018959e8
hntnam3094 <hntnam98@gmail.com>
2020-10-06 16:09:08 +0700

    first commit

---
+++ Project_HelloWorlds/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_HelloWorlds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text =  UpperCase("hello Worlds");
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+
+        static string UpperCase(string text)
+        {
+            return text.ToUpper();
+
+        }
+    }
+}
